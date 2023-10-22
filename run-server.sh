635dccc9218ca2b7cef06252449719df0aca54a0
Oscar Martin <fronix@fronix.se>
2023-10-22 15:36:44 +0200

    Update run-server.sh

--- run-server.sh
+++ run-server.sh
@@ -4,7 +4,7 @@
 # /opt/foundryvtt, unzip and remove it. Then start the foundry app
 
 cd /opt/foundryvtt
-find /host -type f -name [f,F]oundry[vtt,VTT]*.zip -exec cp '{}' . ';'
+sudo find /host -type f -name [f,F]oundry[vtt,VTT]*.zip -exec cp '{}' . ';'
 sudo unzip -o *.zip && rm *.zip
 sudo chown -R fvtt:fvtt /opt/foundryvtt
 node resources/app/main.js --dataPath=/data/foundryvtt
